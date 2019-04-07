5d33091d878169159ad02760d85b321ad9c6fc7f
Jose Jimenez-Olivas <josejimenezjr18@gmail.com>
2019-04-07 01:04:22 -0700

    image and cleaned js

--- website/js/index.js
+++ website/js/index.js
@@ -2,17 +2,7 @@ const fileInput = document.getElementById('file-input');
 fileInput.addEventListener('change', (e) => displayImage(e.target.files));
 
 function displayImage(fileList) {
-  let file = null;
-
-  for(let i = 0; i < fileList.length; i++) {
-    //looking for image type in file list
-    if(fileList[i].type.match(/^image\//)) {
-      file = fileList[i];
-      break;
-    }
-  }
-
-  if(file !== null) {
-    document.getElementById('image-location').style.backgroundImage = "url(" + URL.createObjectURL(file) + ")";
-  }
+  let file = fileList[0];
+  /* sets background image to uploaded file in fileList */
+  document.getElementById('image-location').style.backgroundImage = "url(" + URL.createObjectURL(file) + ")";
 }

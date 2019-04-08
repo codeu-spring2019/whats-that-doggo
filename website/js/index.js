56b284c9649a8ea3b5b4a31274d74d1ca1935f97
gcheng21 <gcheng21@users.noreply.github.com>
2019-04-07 22:57:33 -0700

    Trying to add functionality for POST image to autoML

--- website/js/index.js
+++ website/js/index.js
@@ -1,8 +1,23 @@
 const fileInput = document.getElementById('file-input');
 fileInput.addEventListener('change', (e) => displayImage(e.target.files));
+fileInput.addEventListener('change', (e) => postImageToAutoML(e.target.files))
 
 function displayImage(fileList) {
   let file = fileList[0];
   /* sets background image to uploaded file in fileList */
   document.getElementById('image-location').style.backgroundImage = "url(" + URL.createObjectURL(file) + ")";
+}
+
+function postImageToAutoML(fileList) {
+  let file = fileList[0];
+  fetch('https://us-central1-sp19-codeu-35-7727.cloudfunctions.net/predict', {
+    method: 'POST',
+    body: file
+  }).then(
+    response => response.json() // if the response is a JSON object
+  ).then(
+    success => console.log(success) // Handle the success response object
+  ).catch(
+    error => console.log(error) // Handle the error response object
+  );
 }

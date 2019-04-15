ef9b98664d866d20332d8b66d31dda91ee4682b0
Michela Burns <michelaburns@berkeley.edu>
2019-04-14 22:21:28 -0700

    Copied in the current version of the code

--- function/index.js
+++ function/index.js
@@ -12,22 +12,37 @@ const client = new automl.PredictionServiceClient();
  * @param {Object} res Cloud Function response context.
  *                     More info: https://expressjs.com/en/api.html#res
  */
-exports.predictImage = (req, res) => {
+
+@@ -19,14 +19,29 @@ exports.predictImage = (req, res) => {
 
   try {
   // TODO:
   // 1. Take image from request (req) and POST to Auto ML predict API
   // 2. Handle errors - e.g. the image couldn't be predicted? No label returned?
   // 3. Return the Auto ML label / score back in the response via res.send
-  
+  const projectId = "sp19-codeu-35-7727";
+  const computeRegion = "us_central1";
+  const modelId = “ICN12345”;
+  const filePath = "gs://sp19-codeu-35-7727-vcm/dog_dataset_two/";
+  const scoreThreshold = 0.5;
+
   // https://cloud.google.com/nodejs/docs/reference/automl/0.1.x/v1beta1.PredictionServiceClient#predict
-  //Project = , Location = gs://sp19-codeu-35-7727-vcm/dog_dataset_two, Model = dog_dataset_two
   const formattedName = client.modelPath('sp19-codeu-35-7727', 'gs://sp19-codeu-35-7727-vcm/dog_dataset_two', 'dog_dataset_two');
   const payload = {};
+
+  //const params = {};
+  //if (scoreThreshold) {
+    //params.score_threshold = scoreThreshold;
+  //}
+
+  payload.image = {imageBytes: content};
   const request = {
     name: formattedName,
     payload: payload,
+    //params: params,
   };
+
+
   
   let prediction = await client.predict(request)
   

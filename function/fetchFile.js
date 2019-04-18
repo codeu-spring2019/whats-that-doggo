d6de1d893cfa8d6ae8300ed323fb1cf675c5524f
Michela Burns <michelaburns@berkeley.edu>
2019-04-18 02:30:54 -0700

    Made a new fetch call. This one incorporates aspects of the one I made before, but instead of printing the stringified JSON to the console, this fetch call returns the response and updates .innerText element of  alist with the breed and score of a classification

--- function/fetchFile.js
+++ function/fetchFile.js
@@ -6,6 +6,8 @@
 var url = 'https://us-central1-sp19-codeu-35-7727.cloudfunctions.net/predict';
 var data = { } //An array within the displayImage implementation
 
+
+//Just prints the raw JSON data to the console
 fetch(url, {
   method: 'POST', // or 'PUT'
   body: data, // data can be `string` or {object}!
@@ -15,3 +17,40 @@ fetch(url, {
 }).then(res => res.json())
 .then(response => console.log(JSON.stringify(response))) //Should return the image's predicted label (aka the breed) and the score
 .catch(error => console.error('Error:', error));
+
+
+
+//Handling the JSON body returned
+var myList = document.querySelector('ul'); //Will contain dog breed on one line and score one the next
+
+fetch(url, {
+  method: 'POST', // or 'PUT'
+  body: data, // data can be `string` or {object}!
+  headers:{
+    'Content-Type': 'application/json'
+  }
+})
+.then(function(response) {
+      if (!response.ok) {
+        throw new Error("HTTP error, status = " + response.status);
+      }
+      return response.json();
+    })
+    .then(function(json) {
+      var breed = document.createElement('li');
+      breed.innerHTML = "Breed: " + response.label;
+      myList.appendChild(breed);
+
+      var score = document.createElement('li');
+      score.innerHTML = "Score: " + response.score;
+      myList.appendChild(score);
+    })
+    .catch(function(error) {
+      var p = document.createElement('p');
+      p.appendChild(
+        document.createTextNode('Error: ' + error.message)
+      );
+      document.body.insertBefore(p, myList);
+    });
+
+

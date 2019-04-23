33323b991445cc099b3c27c519e43ffa275f2015
Michela Burns <michelaburns@berkeley.edu>
2019-04-23 01:42:55 -0700

    After the fetch call, we have to return response

--- function/fetchFile.js
+++ function/fetchFile.js
@@ -46,6 +46,7 @@ function makePrediction(file,url) {
       headers:{
         'Content-Type': 'application/json'
       }
+      return response;
     })
     .then(function(response) { //Path #1 from fetch call
           let data = response.json();

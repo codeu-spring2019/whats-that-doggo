e674eb7c995e569644b74c12b9f0d800f645658c
Michela Burns <michelaburns@berkeley.edu>
2019-04-23 01:37:57 -0700

    Integrated aspects of the frontend's js file into the fetchFile.js. Additionally, I made changes to the image upload, made a makePrediction function, and changed the two paths of the fetch call

--- function/fetchFile.js
+++ function/fetchFile.js
@@ -13,50 +13,67 @@
 <span class="p">ErrorMessage: </span><span id="error-message" class="score"></span> //Can access with getElementById
 </div>
 
+//HTML input element with an accept filter that indicates you only want image files
+//Didn't use capture attribute since the user can decide which type of image they want to upload
+<input type="file" accept = "image/*" id = "file-input">
+
+
 
 //Javascript
-var url = 'https://us-central1-sp19-codeu-35-7727.cloudfunctions.net/testPredict';
-var data = { } //An array within the displayImage implementation
+<script>
 
-if (!data.type.match("image/*") {
-    throw new Error("data is not of the type image");
+//File input
+const fileInput = document.getElementById('file-input');
+fileInput.addEventListener('change', (e) => displayImage(e.target.files)); //files property is a FileList object
+
+//Handling a FileList object (aka the uploaded image)
+
+function displayImage(fileList) {
+  let file = fileList[0]; //Since we only allow them to upload one image
+
+  /* sets background image to uploaded file in fileList */
+  document.getElementById('image-location').style.backgroundImage = "url(" + URL.createObjectURL(file) + ")";
 }
 
 
+var url = 'https://us-central1-sp19-codeu-35-7727.cloudfunctions.net/testPredict';
 
-fetch(url, {
-  method: 'POST', // or 'PUT'
-  body: data, // data can be `string` or {object}!
-  headers:{
-    'Content-Type': 'application/json'
-  }
-})
-.then(function(response) {
-      if (!response.ok) {
-        throw new Error("HTTP error, status = " + response.status);
+function makePrediction(file,url) {
+    //Calls fetch on the uploaded image
+    fetch(url, {
+      method: 'POST',
+      body: file, // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json'
       }
-      return response.json();
     })
-    .then(function(json) {
-      //Get the breed (which is stored in the response's label)
-      let breedElement = document.getElementById("dog-breed");
-      breedElement.innerText = response.label;
+    .then(function(response) { //Path #1 from fetch call
+          let data = response.json();
 
-      //Get the image's confidence score (store in the response's score)
-      let scoreElement = document.getElementById("prediction-score");
-      scoreElement.innerText = response.score
+          //Update (not creates, unhide if needed) the HTML elements
 
-      let resultDiv = document.getElementById("prediction-results")
-      resultDiv.style.display = "Breed: " + resultDiv.getElementById("dog-breed") + ", Score: " + getElementById("prediction-score");
+          //Get the breed (which is stored in the response's label)
+          let breedElement = document.getElementById("dog-breed");
+          breedElement.innerText = data.label;
+
+          //Get the image's confidence score (store in the response's score)
+          let scoreElement = document.getElementById("prediction-score");
+          scoreElement.innerText = data.score
+
+          let resultDiv = document.getElementById("prediction-results")
+          resultDiv.style.display = "Breed: " + resultDiv.getElementById("dog-breed") + ", Score: " + getElementById("prediction-score");
 
 
     })
-    .catch(function(error) {
-      let error_message = document.getElementById("error-message");
-      error_message.innerText = "Error: " + error.message;
+    .catch(function(err) { //Path #2 from fetch call
+          JSON.stringify(err); //Unwraps the error object
 
-      let errorDiv = document.getElementById("prediction-error");
-      errorDiv.style.display = errorDiviv.getElementById("error-message");
+          let error_message = document.getElementById("error-message");
+          error_message.innerText = "Error: " + err.error;
+
+          let errorDiv = document.getElementById("prediction-error");
+          errorDiv.style.display = errorDiviv.getElementById("error-message");
     });
 
-
+}
+</script>

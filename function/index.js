1dd8b8820f6df0b90be3fd2b1fcbb23c60af0d51
Matt Silverlock <matt@eatsleeprepeat.net>
2019-04-23 09:38:20 +0100

    Function correctly handles multipart image uploads

--- function/index.js
+++ function/index.js
@@ -20,71 +20,90 @@ const modelFullId = client.modelPath(projectId, computeRegion, modelId)
  *                     More info: https://expressjs.com/en/api.html#res
  */
 exports.predict = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.set("Access-Control-Allow-Origin", "*")
+    res.set(
+      "Access-Control-Allow-Headers",
+      "Accept, Accept-Language, Content-Language, Content-Type"
+    )
+    res
+      .status(200)
+      .send()
+      .end()
+    return
+  }
+
+  if (req.method !== "POST") {
+    res
+      .status(405)
+      .send()
+      .end()
+    return
+  }
+
+  let busboy = new Busboy({
+    headers: req.headers,
+    // Only accept one file upload
+    limits: {
+      fileSize: 1024 * 1024 * 1024 * 10, // 10MB,
+      files: 1
+    }
+  })
+
+  // Handle the image upload
+  let imageUpload = []
+  busboy.on("file", (fieldname, file, filename, enc, mimeType) => {
+    console.log(
+      `received file: ${fieldname}=${filename} - encoding=${enc} - mimetype=${mimeType}`
+    )
+    // Validate the MIME type (must be an image - e.g. image/png, image/jpg, etc)
+    if (!mimeType.includes("image")) {
+      res
+        .status(400)
+        .json({ error: `upload must be an image: got ${mimeType}` })
+        .end()
+      return
+    }
+
+    file.on("data", function(data) {
+      console.log(`file upload '${fieldname}' - length: ${data.length}`)
+      // We have to append each "chunk" received from the client, and stitch
+      // together after.
+      imageUpload.push(data)
+    })
+  })
+
+  busboy.on("finish", () => {
+    // Concatenate all chunks into a single Buffer type (a byte array).
+    // Since images sent to the AutoML API must be also base64-encoded, we transform
+    // the Buffer to a base64-encoded string.
+    let image = Buffer.concat(imageUpload).toString("base64")
+    predictImage(req, res, image)
+  })
+
+  busboy.end(req.rawBody)
+}
+
+/*
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ * @param {string} image Image data as a base64-encoded string.
+ */
+function predictImage(req, res, image) {
+  console.log("predicting image")
   try {
-    if (req.method === "OPTIONS") {
-      res.set("Access-Control-Allow-Origin", "*")
-      res.set(
-        "Access-Control-Allow-Headers",
-        "Accept, Accept-Language, Content-Language, Content-Type"
-      )
-      res
-        .status(200)
-        .send()
-        .end()
-      return
-    }
-
-    if (req.method !== "POST") {
-      res
-        .status(405)
-        .send()
-        .end()
-      return
-    }
-
-    let busboy = new Busboy({
-      headers: req.headers,
-      limits: {
-        fileSize: 1024 * 1024 * 1024 * 10, // 10MB,
-        files: 1
-      }
-    })
-
-    let imageUpload
-
-    // Handle the image upload
-    // 1. Validate the MIME type (is it an image?)
-    // 2. Check the length (is it too large)
-    // 3. Prepare it for prediction
-    busboy.on("file", (fieldname, file, filename, enc, mimeType) => {
-      console.log(`received file: ${fieldname}=${filename}`)
-      file.on("data", function(data) {
-        console.log("File [" + fieldname + "] got " + data.length + " bytes")
-      })
-      file.on("end", function() {
-        console.log("File [" + fieldname + "] Finished")
-      })
-
-      imageUpload = file
-    })
-
-    busboy.on("finish", () => {
-      console.log("upload complete")
-    })
-
-    req.pipe(busboy)
-
-    // After upload finishes
+    // Build our API request - a params object that defines the cut-off
+    // confidence level, and a payload object that contains base64-encoded image
+    // bytes.
     let params = {}
     if (scoreThreshold) {
       params.score_threshold = scoreThreshold
     }
 
     let payload = {
-      image: { imageBytes: imageUpload }
+      image: { imageBytes: image }
     }
 
-    let prediction
     console.log(`using model at: ${modelFullId}`)
     client
       .predict({
@@ -93,35 +112,46 @@ exports.predict = (req, res, next) => {
         params: params
       })
       .catch(err => {
-        throw new Error(err)
+        console.log(err)
+        res
+          .status(500)
+          .json({ error: err })
+          .end()
+        return
       })
-      .then(res => {
-        prediction = res
+      .then(prediction => {
+        console.log("prediction response returned")
+        // Docs: https://cloud.google.com/nodejs/docs/reference/automl/0.1.x/google.cloud.automl.v1beta1#.PredictResponse
+        // Get only the first prediction response
+        let data = prediction[0]["payload"]
+
+        // Build our response object
+        let result = {
+          label: data[0].displayName,
+          score: data[0].classification.score
+        }
+
+        res.set("Access-Control-Allow-Origin", "*")
+        res
+          .status(200)
+          .json(result)
+          .end()
+        return
       })
       .catch(err => {
-        throw new Error(err)
+        console.log(err)
+        res
+          .status(500)
+          .json({ error: err })
+          .end()
+        return
       })
-
-    // Docs: https://cloud.google.com/nodejs/docs/reference/automl/0.1.x/google.cloud.automl.v1beta1#.PredictResponse
-    console.log(prediction.payload[0])
-    let result = {
-      label: prediction.payload[0].displayName,
-      score: prediction.payload[0].classification.score
-    }
-
-    res.set("Access-Control-Allow-Origin", "*")
-    res
-      .status(200)
-      .json(result)
-      .end()
-    return
   } catch (err) {
     // Handle errors explicitly
-    let errMsg = `fatal: ${JSON.stringify(err)}`
-    console.log(errMsg)
+    console.log(err)
     res
       .status(500)
-      .json({ error: errMsg })
+      .json({ error: err })
       .end()
     return
   }
